950fd9d34a7a7431adcbaa3c97845724388b29dd
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-07-28 00:36:37 +0100

    Add toggle terminal keymap

--- init.lua
+++ init.lua
@@ -69,32 +69,6 @@ vim.o.shellslash = true
 -- Highlight column when line is too long.
 vim.o.colorcolumn = "88"
 
--- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- Diagnostic keymaps
-vim.keymap.set(
-    "n",
-    "<leader>q",
-    vim.diagnostic.setloclist,
-    { desc = "Open diagnostic [Q]uickfix list" }
-)
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- vim.keymap.set('i', '<C-\'>', '<Esc><:vs><:term><:i>', { desc = 'Open terminal in split window' })
--- vim.keymap.set('n', '<C-h>', '<:vs>', { desc = 'Open terminal in split window' })
-
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
@@ -105,6 +79,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Configure and install plugins
-require("config.lazy")
-
+require("config")
 require("nvim-treesitter.install").compilers = { "zig" }
