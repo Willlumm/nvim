b7489a3fde9123eec9457713edd95281489ecbfb
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-07-27 12:49:19 +0100

    Split init.lua into separate files

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,5 @@
+-- https://github.com/folke/todo-comments.nvim
+-- Highlight and search for TODO comments
+
+return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+
