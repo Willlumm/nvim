d0bdf957d3ddaa736b9c77adfada2ac3a03f9ab7
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-08-09 21:06:02 +0100

    Add Ruff LSP

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -174,6 +174,7 @@ return {
                     },
                 },
             },
+            ruff = {},
         }
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
